95c06dc33e31b434b85a58121c1871097661c55e
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-09-27 11:12:15 +0000

    Add api.py to routes

---
+++ ThinkFlash-BackEnd/src/routes/api.py
@@ -0,0 +1,7 @@
+#from flask import Flask, request, jsonify, url_for, Blueprint
+#from api.models import db, User
+#from api.utils import generate_sitemap
+
+#api = Blueprint('api', __name__)
+
+#api.register_blueprint(api)
