cc5f6bb3e4573ece5da71a2950e442527c285d20
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-10-05 15:09:20 +0000

    miau

--- ThinkFlash-BackEnd/src/app.py
+++ ThinkFlash-BackEnd/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.users import users
 from flask_migrate import Migrate
 from models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.sqlite'
@@ -9,6 +10,9 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')  # Change this!
+jwt = JWTManager(app)
 
 migrate = Migrate(app, db)
 
