8c45be79b82d46c45e806f7b9b1f0aaa175f1dec
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-10-06 13:58:22 +0000

    homepage-navbar.v1

--- ThinkFlash-FrontEnd/src/components/Navbar/Navbar.jsx
+++ ThinkFlash-FrontEnd/src/components/Navbar/Navbar.jsx
@@ -2,10 +2,43 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({usernameLink}) {
     return (
-        <nav className="navbar bg-primary">
-                <Link to={usernameLink} className="navbar-brand ps-3">
-                    <img src='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png' alt="Logo" width="auto" height="100"/>
-                </Link>
-        </nav>
+        <div className="navbar bg-primary navbar-expand-lg" data-bs-theme="dark" >
+        <div className="container">
+        <Link to={usernameLink} className="navbar-brand ps-3">
+            <img src='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png' alt="Logo" width="auto" height="100px"/>
+        </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Demo</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Product</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">Contact</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">User</a>
+                        <ul className="dropdown-menu bg-primary">
+                            <li><a className="dropdown-item" href="#">Log in</a></li>
+                            <li><a className="dropdown-item" href="#">Sign up</a></li>
+                            <li><a className="dropdown-item" href="#">Log out</a></li>
+                        </ul>
+                    </li>
+                </ul>  
+            </div>
+        </div>
+    </div>
+       
     )
 }
+
+
+
--- ThinkFlash-FrontEnd/src/pages/HomePage.jsx
+++ ThinkFlash-FrontEnd/src/pages/HomePage.jsx
@@ -1,5 +1,66 @@
 export default function HomePage () {
     return(
-        <h1>This is a home page</h1>
+		<div>
+			<header className="header mt-3 pt-7">
+				<div className="container-xl">
+					<div className="row">
+						<div className="col-md-6">
+							<div className="image-container mb-5 px-4">
+							<img src="https://i.ibb.co/rtBZyJW/8-Online-Learning-isolated.png" alt="" className="img-fluid"/>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="text-container p-4 d-flex flex-column justify-content-center h-100 mb-5 text-center">
+								<h1 className="display-3">Lorem, ipsum.</h1>
+								<p className="lead text-secondary mt-3">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos est dolore nemo veritatis, quisquam quo
+									facere assumenda enim, odio necessitatibus vitae, inventore ea quibusdam fugiat.
+								</p>
+								<div class="container text-center mt-5">
+								<button className="btn btn-primary col-lg-3">Star here!</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div id="features" class="py-5">
+			<div class="container">
+				<div class="row g-5 py-5">
+					<div class="col-md-4">
+						<h5 class="fw-bold">Lorem, ipsum dolor.</h5>
+						<p class="text-muted text-justify">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores, et officiis omnis officia
+							laudantium eos?
+						</p>
+					</div>
+					<div class="col-md-4">
+						<h5 class="fw-bold">Lorem, ipsum dolor.</h5>
+						<p class="text-muted text-justify">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores, et officiis omnis officia
+							laudantium eos?
+						</p>
+					</div>
+					<div class="col-md-4">
+						<h5 class="fw-bold">Lorem, ipsum dolor.</h5>
+						<p class="text-muted text-justify">
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores, et officiis omnis officia
+							laudantium eos?
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer class="container p-1">
+			<p class="text-center small text-muted">Copyright &copy; Think Flash 2023</p>
+		</footer>
+
+
+
+		</div>
+
+		
+
     )
 }
