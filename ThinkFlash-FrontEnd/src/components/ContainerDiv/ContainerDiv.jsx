0cd334dcf9ee63d49ccf9ef8ba7aea0c01f00a51
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-10-10 10:48:20 +0000

    Scroll bar with style

--- ThinkFlash-FrontEnd/src/components/ContainerDiv/ContainerDiv.jsx
+++ ThinkFlash-FrontEnd/src/components/ContainerDiv/ContainerDiv.jsx
@@ -4,13 +4,13 @@ export default function ContainerDiv({ children, height, title, link, container 
     return (
             <div className={`h-${height} w-100 my-3`}>
                 <div className="card border border-0 h-25">
-                    <div className="w-100 card-header border-bottom align-baseline d-flex justify-content-between align-items-end">
-                        <p className="mb-0 mt-0">{title}</p>
+                    <div className="w-100 card-header border-bottom align-baseline d-flex justify-content-between align-items-end bg-white">
+                        <h1 className="mb-0 mt-0">{title}</h1>
                         {link && <Link to={link} className="ms-auto me-0 fs-7 text-body-secondary text-end">View all</Link>}
                     </div>
                     </div>
                     
-                        <div className="overflow-x-auto card-body h-75 d-flex ">
+                        <div className="overflow-x-auto card-body h-75 d-flex containerDiv">
                             {children}
                         </div>
             </div>
