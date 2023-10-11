2b8c5c5d35a628299222b09e4a0372178dc9c1bb
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-10-11 12:32:35 +0000

    Fixing errors in generalCard

--- ThinkFlash-FrontEnd/src/pages/MyDecksPage.jsx
+++ ThinkFlash-FrontEnd/src/pages/MyDecksPage.jsx
@@ -2,6 +2,7 @@ import ContainerDiv from "../components/ContainerDiv"
 import GeneralCard from "../components/GeneralCard/GeneralCard"
 import getDecks from "../services/decks/getDecks"
 import { allDecksData } from "../../constants"
+import "../../style.css"
 
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
@@ -34,7 +35,7 @@ export default function MyDecksPage() {
       <ContainerDiv title="My Decks" overflow="y">
         {deckList.map((deck, index) => 
           (
-            <GeneralCard key={index} title={deck.specialize} col="col" minWidth="15rem" minHeight="20rem"
+            <GeneralCard key={index} title={deck.specialize} minWidth="15rem" minHeight="20rem"
             img="https://img.asmedia.epimg.net/resizer/LQyBk5T2TfVttC_yVM8n5HuEYpM=/1472x828/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/53YSJXSIZFHNTBV52Z4AMKISUM.png">
               {deck.theme}
             </GeneralCard>
