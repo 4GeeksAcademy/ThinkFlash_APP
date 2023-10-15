90061f056c09fd9b2e4c4946bf036cee6803c171
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-10-15 16:24:14 +0000

    circle advance

--- Front/src/components/GeneralCard/GeneralCard.jsx
+++ Front/src/components/GeneralCard/GeneralCard.jsx
@@ -1,7 +1,7 @@
 import GetProgress from '../../../utils/GetProgress';
 import GetImage from '../../../utils/GetImage';
-import React, { useEffect, useRef } from 'react';
-import "../../styles/generalCard.css";
+import React from 'react';
+
 
 export default function GeneralCard({ children, title, shadow, minWidth, minHeight, img, progress }) {
 
@@ -9,8 +9,10 @@ export default function GeneralCard({ children, title, shadow, minWidth, minHeig
         <div className={`m-2 d-flex justify-content-center align-items-center`} style={{ minHeight: minHeight, minWidth: minWidth, maxHeight: minHeight, maxWidth: minWidth }}>
             <div className="d-flex flex-wrap" style={{ minHeight: minHeight, minWidth: minWidth }}>
                 <div className={`card shadow${shadow}`} style={{ minHeight: minHeight, minWidth: minWidth }}>
+                   <div className="container">
                     {img && GetImage(img)}
                     {progress && GetProgress({progress:progress})}
+                    </div> 
                     <h5 className="card-title m-2 text-center">{title}</h5>
                     <div className="card-body">
                         <div className="h-100 d-flex flex-column justify-content-center align-item-center text-center">{children}</div>
--- Front/utils/GetProgress.jsx
+++ Front/utils/GetProgress.jsx
@@ -1,57 +1,106 @@
-import { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "../src/styles/getProgress.css";
 
-const GetProgress = ({progress}) => {
+
+const GetProgress = ({ progress }) => {
+    const [clicked, setClicked] = useState(false);
     const canvasRef = useRef(null);
     const chartRef = useRef(null);
 
     const { learned, midLearned, toLearn } = progress;
 
     useEffect(() => {
-        const ctx = canvasRef.current.getContext('2d');
+        if (canvasRef.current) {
+            const ctx = canvasRef.current.getContext("2d");
 
-        const data = {
-            labels: ['Learned', 'Mid Learned', 'To Learn'],
-            datasets: [{
-                data: [learned, midLearned, toLearn],
-                backgroundColor: ['green','yellow', 'red'],
-                borderWidth: 0,
-            }],
-        };
-
-        if (chartRef.current) {
-            chartRef.current.destroy();
-        }
-
-        const newChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: data,
-            options: {
-                cutout: '80%',
-                plugins: {
-                    legend: {
-                        display: false,
+            const data = {
+                labels: ["Learned", "Mid Learned", "To Learn"],
+                datasets: [
+                    {
+                        data: [learned, midLearned, toLearn],
+                        backgroundColor: ["#4caf50", "#ff9800", "#f44336"],
+                        borderWidth: 1,
                     },
-                },
-            },
-        });
+                ],
+            };
 
-        chartRef.current = newChart;
-
-        return () => {
             if (chartRef.current) {
                 chartRef.current.destroy();
             }
-        };
-    }, [learned, midLearned, toLearn]);
+
+            if (clicked) {
+                const barData = {
+                    labels: ["Learned", "Mid Learned", "To Learn"],
+                    datasets: [
+                        {
+                            label: "Percentage",
+                            data: [learned, midLearned, toLearn],
+                            backgroundColor: ["#4caf50", "#ff9800", "#f44336"],
+                            borderWidth: 3,
+                        },
+                    ],
+                };
+
+            } else {
+                const newChart = new Chart(ctx, {
+                    type: "doughnut",
+                    data: data,
+                    options: {
+                        cutout: "70%",
+                        plugins: {
+                            legend: {
+                                display: false,
+                            },
+                        },
+                    },
+                });
+
+                chartRef.current = newChart;
+            }
+
+            return () => {
+                if (chartRef.current) {
+                    chartRef.current.destroy();
+                }
+            };
+        }
+    }, [clicked, learned, midLearned, toLearn]);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
     return (
-        <div className='ratio ratio-4x3 d-block'>
-            <div className='w-100 h-75 mt-4 mb-0'>
-                <div className='circulo w-100 h-100 d-flex justify-content-center align-items-center'>
-                    <canvas ref={canvasRef}></canvas>
+        <div className="ratio ratio-4x3 d-block">
+            <div className="w-100 h-75 mt-4 mb-0" onClick={handleClick}>
+                <div className=" w-100 h-100 d-flex justify-content-center align-items-center">
+                    {clicked ? (
+
+                        <div className="container progressBarContainer ">
+                            <div className="progressBarLabelGreen"> Learned: {learned}%</div>
+                            <div className="progressBarLabelOrange">Mid Learned: {midLearned}%</div>
+                            <div className="progressBarLabelRed">To Learn: {toLearn}%</div>
+                            <div className="additionalInfo">
+                            <p><strong>Click para menos info!</strong></p>
+                            </div>
+
+
+                        </div>
+                    ) : (
+                        <>
+                            <canvas ref={canvasRef}></canvas>
+                            <div className="additionalInfo">
+                                <p> <strong>Click para + info!</strong></p>
+                            </div>
+
+                        </>
+
+                    )}
                 </div>
             </div>
         </div>
     );
 };
 
-export default GetProgress
+export default GetProgress;
