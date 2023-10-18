838fd26512567b31018ad9ecdbb018ef12f9ffce
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-10-18 17:33:05 +0000

    creacion de nueva rama y prueba

--- Front/src/pages/HomePage.jsx
+++ Front/src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ export default function HomePage () {
 								personalizar tu experiencia seleccionando tus barajas favoritas o creando las tuyas propias. Think Flash combina la seriedad 
 								del estudio con la flexibilidad necesaria para adaptarse a tus necesidades educativas desde todos tus dispositivos.</p>
 								<div className="container text-center mt-3">
-								<button className="btn btn-lg fw-bold text-bg-dark col-lg-5">¡Comienza ahora!</button>
+								<button className="btn btn-lg fw-bold text-white bg-info col-lg-5">¡Comienza ahora!</button>
 								</div>
 							</div>
 						</div>
@@ -32,19 +32,19 @@ export default function HomePage () {
 			<div className="container-xl ">
 				<div className="row p-4 g-5 py-5">
 					<div className="col-md-4">
-						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-dark mb-4">Aprendizaje Personalizado 🧠</h5>
+						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-info mb-4">Aprendizaje Personalizado 🧠</h5>
 						<p className="text-muted text-center">
 						Adapta tu experiencia de estudio según tus necesidades. Personaliza tus barajas, elige tus temas favoritos y dirige tu camino hacia el conocimiento, convirtiendo cada sesión de estudio en una experiencia única y efectiva.
 						</p>
 					</div>
 					<div className="col-md-4">
-						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-dark mb-4"> Medición Precisa de Progreso 📈</h5>
+						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-info mb-4"> Medición Precisa de Progreso 📈</h5>
 						<p className="text-muted text-center">
 						Mide tu progreso de manera precisa con cada carta acertada. Observa cómo tu conocimiento se expande y aprovecha la retroalimentación instantánea para perfeccionar tus habilidades.
 						</p>
 					</div>
 					<div className="col-md-4">
-						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-dark mb-4">Flexibilidad en el Aprendizaje 🚀</h5>
+						<h5 className="font-weight-normal rounded-3 p-2 text-center text-white bg-info mb-4">Flexibilidad en el Aprendizaje 🚀</h5>
 						<p className="text-muted text-center">
 						Desde oposiciones públicas hasta idiomas, Think Flash abarca un amplio espectro de temas. Cambia entre barajas y temáticas, adaptándote a tus objetivos educativos.
 						</p>
@@ -52,7 +52,7 @@ export default function HomePage () {
 				</div>
 			</div>
 			<footer>
-				<div className="text-center mt-3 p-4 text-white bg-dark">
+				<div className="text-center mt-3 p-4 text-white bg-info">
     				© 2023 Think Flash 2023
   				</div>
 			</footer>
