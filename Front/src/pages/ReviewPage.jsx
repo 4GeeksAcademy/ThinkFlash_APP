0cbf381c13c0f805b89072bc970611d58d265d54
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-10-26 11:32:30 +0000

    Making author cards only visible for the author

--- Front/src/pages/ReviewPage.jsx
+++ Front/src/pages/ReviewPage.jsx
@@ -20,93 +20,91 @@ export default function ReviewPage() {
 
     const params = useParams();
     const deck_id = params.deck_id;
-    const username = params.username; 
+    const username = params.username;
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setIsLoading(true);
-                const res = await getDeckCards(id, deck_id);
-                setCardList(res.cards);
-                setIsLoading(false);
-            } catch (error) {
-                console.error('Error fetching decks:', error);
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
+        getDeckCards(id, deck_id)
+            .then((res) => {
+                console.log("esta funcionando", res)
+                setCardList(res)
+                return res
+            })
+            .then((res)=>{
+                setToLearnCardList(res.filter(card => card.score === 1));
+                setMidLearnedCardList(res.filter(card => card.score > 1 && card.score < 4));
+                setLearnedCardList(res.filter(card => card.score === 4));
+            })
+            .then(() => {
+                setIsLoading(false)
+            })
+            .catch((err) => {
+                console.log("Error fetch in review page", err)
+            })
     }, [deck_id, id]);
 
-useEffect(() => {
-    setToLearnCardList(cardList.filter(card => card.score === 1));
-    setMidLearnedCardList(cardList.filter(card => card.score > 1 && card.score < 4));
-    setLearnedCardList(cardList.filter(card => card.score === 4));
-}, [cardList]);
+    console.log("card list", cardList)
+    console.log("learned list", learnedCardList)
+    console.log("midlearnedList", midLearnedCardList)
+    console.log("tolearnlist", toLearnCardList)
 
-console.log("card list", cardList)
-console.log("learned list", learnedCardList)
-console.log("midlearnedList", midLearnedCardList)
-console.log("tolearnlist", toLearnCardList)
-
-const getToNewCardButton = () => {
-    return (<button className={`btn btn-${colorMode} float-end me-3 mb-1`} onClick={()=> navigate(`/${username}/${deck_id}/create_card`)}>
-      <i class="fas fa-plus me-1"></i> New Card
-    </button>)
+    const getToNewCardButton = () => {
+        return (<button className={`btn btn-${colorMode} float-end me-3 mb-1`} onClick={() => navigate(`/${username}/${deck_id}/create_card`)}>
+            <i class="fas fa-plus me-1"></i> New Card
+        </button>)
     }
 
 
-const colorMode = getPreferentColor();
+    const colorMode = getPreferentColor();
 
-return (
-    <div className="h-auto container">
-         <ContainerDiv 
-         title="To Learn Cards" 
-         titleButton={getToNewCardButton()}
-         overflow="y">
-            {toLearnCardList.map((card, index) => {
-                return(
-                    <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
-                    className=""
-                    img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
-                        {card.description}
-                    </GeneralCard>
-                )
-            })
+    return (
+        <div className="h-auto container">
+            <ContainerDiv
+                title="To Learn Cards"
+                titleButton={getToNewCardButton()}
+                overflow="y">
+                {toLearnCardList.map((card, index) => {
+                    return (
+                        <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
+                            className=""
+                            img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
+                            {card.description}
+                        </GeneralCard>
+                    )
+                })
 
-            }
+                }
 
-        </ContainerDiv>
-        <ContainerDiv title="MidLearned Cards" overflow="y">
-            {midLearnedCardList.map((card, index) => {
-                return(
-                    <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
-                    className=""
-                    img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
-                        {card.description}
-                    </GeneralCard>
-                )
-            })
+            </ContainerDiv>
+            <ContainerDiv title="MidLearned Cards" overflow="y">
+                {midLearnedCardList.map((card, index) => {
+                    return (
+                        <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
+                            className=""
+                            img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
+                            {card.description}
+                        </GeneralCard>
+                    )
+                })
 
-            }
+                }
 
-        </ContainerDiv>
-        <ContainerDiv title="Learned Cards" overflow="y">
-            {learnedCardList.map((card, index) => {
-                return(
-                    <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
-                    className=""
-                    img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
-                        {card.description}
-                    </GeneralCard>
-                )
-            })
+            </ContainerDiv>
+            <ContainerDiv title="Learned Cards" overflow="y">
+                {learnedCardList.map((card, index) => {
+                    return (
+                        <GeneralCard key={index} title={card.concept} minWidth="15rem" minHeight="20rem" shadow={"-lg"}
+                            className=""
+                            img='https://i.ibb.co/Phs1CSV/Logo-2-removebg-preview.png'>
+                            {card.description}
+                        </GeneralCard>
+                    )
+                })
 
-            }
+                }
 
-        </ContainerDiv>
-    </div>
-);
+            </ContainerDiv>
+        </div>
+    );
 }
