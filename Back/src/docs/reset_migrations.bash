05a87798a27393d7d6dc2b7073b4e55953620ee7
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-11-20 20:56:03 +0000

    removed dropdb

--- Back/src/docs/reset_migrations.bash
+++ Back/src/docs/reset_migrations.bash
@@ -11,18 +11,18 @@ export DB_USER="thinkflash"
 export DB_PASSWORD="4KAa5C7iifA7FhhRdQSa6ZzBbanvtp7g"
 export DB_NAME="thinkflashdatabase"
 
-# Operaciones relacionadas con la base de datos
-{
-  # Intentar eliminar y crear la base de datos
-  dropdb -h $DB_HOST -U $DB_USER -e $DB_NAME || true
-  createdb -h $DB_HOST -U $DB_USER -e $DB_NAME || true
+# # Operaciones relacionadas con la base de datos
+# {
+#   # Intentar eliminar y crear la base de datos
+#   dropdb -h $DB_HOST -U $DB_USER -e $DB_NAME || true
+#   createdb -h $DB_HOST -U $DB_USER -e $DB_NAME || true
 
-  # Crear extensión unaccent si no existe
-  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -e -c 'CREATE EXTENSION IF NOT EXISTS unaccent;' || true
-} || {
-  echo "Error en las operaciones de la base de datos"
-  exit 1
-}
+#   # Crear extensión unaccent si no existe
+#   PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -e -c 'CREATE EXTENSION IF NOT EXISTS unaccent;' || true
+# } || {
+#   echo "Error en las operaciones de la base de datos"
+#   exit 1
+# }
 
 # Ejecutar migraciones y actualizar la base de datos
 pipenv run flask db migrate
